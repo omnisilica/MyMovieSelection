7193f18ad7c7541789a5abe6cd08d4b5cee8a391
omnisilica <oxmaverix@gmail.com>
2022-09-03 22:42:40 -0400

    First Commit. Start MyMovieSelection Project

---
+++ src/main/java/com/omnisilica/mymovieselection/MyMovieSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.omnisilica.mymovieselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovieSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovieSelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnisilica/mymovieselection/MyMovieSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnisilica.mymovieselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
